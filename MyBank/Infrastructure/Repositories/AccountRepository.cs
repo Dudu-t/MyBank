53b27aea94ba79788ba9981216f36d3872f144a4
Carlos Eduardo <carloslage.dev@gmail.com>
2023-04-19 01:26:59 -0300

    Add: added constructor and dependecy injector

--- MyBank/Infrastructure/Repositories/AccountRepository.cs
+++ MyBank/Infrastructure/Repositories/AccountRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyBank.Domains.Entities;
+using MyBank.Domains.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,48 @@ using System.Threading.Tasks;
 
 namespace MyBank.Infrastructure.Repositories
 {
-    public class AccountRepository
+    public class AccountRepository : IAccountRepository
     {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountRepository(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        public void Add(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Account> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetByAgencyAndNumber(int agency, int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetByCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Account account)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
